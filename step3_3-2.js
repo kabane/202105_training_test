3f828674d258a7521dd7a0832f919f020c4cfa7f
Mashimo Takuya <t_mashimo@uluru561.local>
2021-05-07 11:48:06 +0900

    3-2の問題の解答を記入

--- step3_3-2.js
+++ step3_3-2.js
@@ -1,5 +1,5 @@
 var devLang = ["JavaScript", "Ruby", "PHP", "Java", "C"];
-devLang[4] = "C+";
+var matchString = "Java";
 for (let i = 0; i < devLang.length; i++) {
   if (devLang[i].includes(matchString)) {
     console.log(devLang[i]);
